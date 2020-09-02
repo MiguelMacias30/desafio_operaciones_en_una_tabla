271d408b43aa52d80a9be0554d4161df83405485
Miguel Macias <miguelfernandomacias@gmail.com>
2020-09-01 23:46:19 -0400

    parte2

--- operaciones.sql
+++ operaciones.sql
@@ -10,3 +10,10 @@ UPDATE post SET titulo='eduacion distancia' WHERE id=3;
 INSERT INTO post(nombre_usuario, fecha_creacion, contenido, descripcion, titulo) VALUES('pedro', '2020-09-03', 'columna_opinion', 'opiniones sobre la afectacion de la pandemia', 'pandemia'), ('pedro', '2020-09-04', 'resumen', 'situacion economica del pais', 'economia actual');
 DELETE FROM post WHERE id=3;
 INSERT INTO post(nombre_usuario, fecha_creacion, contenido, descripcion, titulo) VALUES('carlos', '2020-09-05', 'articulo', 'desempeño maestros virtuales', 'educacion');
+--parte 2
+CREATE TABLE comentarios(id INT, fecha TIMESTAMP, contenido VARCHAR(70));
+ALTER TABLE post ADD PRIMARY KEY(id);
+ALTER TABLE comentarios ADD FOREIGN KEY(id) REFERENCES post(id);
+INSERT INTO comentarios (id, fecha, contenido) VALUES(1, '2020-09-08 10:30:00', 'me parece una burla al rio bravo'), (2, '2020-10-11 11:45:30', 'me gusta la forma de escribir'), (6, '2020-09-15 12:30:45', 'los maestros deben mejorar'), (6, '2020-09-14 10:30:20', 'espero el material sea suficiente'), (6, '2020-08-10 11:40:30', 'el proyecto de eduacion no convence'), (6, '2020-09-20 12:30:05', 'el proyecto tiene vacios');
+INSERT INTO post(nombre_usuario, fecha_creacion, contenido, descripcion, titulo) VALUES('margarita', '2020-08-15', 'articulo', 'investigaciones sobre la posibilidad de vida en marte', 'vida en marte');
+INSERT INTO comentarios (id, fecha, contenido) VALUES(7, '2020-09-08 10:30:00', 'me parece una burla la vida en marte'), (7, '2020-10-11 11:45:30', 'me gusta la forma de escribir'), (7, '2020-09-15 12:30:45', 'los marcianos deben mejorar'), (7, '2020-09-14 10:30:20', 'espero el material sea suficiente'), (7, '2020-08-10 11:40:30', 'el proyecto de los marcianos no me convence');
